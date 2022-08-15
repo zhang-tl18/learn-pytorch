e6857997f0e246377ca9b42553c354e01417b47a
Tianle Zhang <2446588658@qq.com>
2022-08-15 20:46:34 +0800

    classifier

---
+++ classifier/classifier.py
@@ -0,0 +1,102 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import time
+
+class Net(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        # 输入图像是3通道，conv1 kenrnel size=5*5，输出通道 6
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        # conv2 kernel size=5*5, 输出通道 16
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        # 全连接层
+        self.fc1 = nn.Linear(16*5*5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+    
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16*5*5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+if __name__ == '__main__':
+    # 加载和归一化 CIFAR10
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+        transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))]
+    )
+
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, 
+                                    download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                    shuffle=True, num_workers=2)
+
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                    download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                    shuffle=False, num_workers=2)
+
+    classes = ('plane', 'car', 'bird', 'cat', 'deer', 
+                'dog', 'frog', 'horse', 'ship', 'truck')
+
+    # 训练
+    net = Net()
+
+    # 定义损失函数和优化器
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+    # 训练网络
+    start = time.time()
+    for epoch in range(2):
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # 获取输入数据
+            inputs, labels = data
+            # 清空梯度缓存
+            optimizer.zero_grad()
+
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # 打印统计信息
+            running_loss += loss.item()
+            if i % 2000 == 1999:
+                # 每 2000 次打印一次信息
+                print('[%d, %5d] loss: %.3f' % (epoch+1, i+1, running_loss / 2000))
+                running_loss = 0.0
+    print('Finished Training! Total cost time: ', time.time()-start)
+
+    # 测试模型性能
+    correct = 0
+    total = 0
+    class_correct = list(0. for i in range(10))
+    class_total = list(0. for i in range(10))
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs, 1)
+            c = (predicted == labels).squeeze()
+            total += labels.size(0)
+            correct += c.sum().item()
+            for i in range(4):
+                label = labels[i]
+                class_correct[label] += c[i].item()
+                class_total[label] += 1
+    
+    print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
+    for i in range(10):
+        print('Accuracy of %5s : %2d %%' % (classes[i], 100 * class_correct[i] / class_total[i]))
